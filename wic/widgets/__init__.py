38a44e122151f80c4a4011ded907ae84005a4056
Victor Varvariuc <victor.varvariuc@gmail.com>
2011-12-04 17:41:44 +0200

    added a note when resources are not imported

--- wic/widgets/__init__.py
+++ wic/widgets/__init__.py
@@ -1,1 +1,4 @@
-from . import w_widgets_rc # load resources (icons, etc.)
+try:
+    from . import w_widgets_rc # load resources (icons, etc.)
+except ImportError:
+    print('Looks like resources are not compiled. Please run `compile_resources.py`.')
